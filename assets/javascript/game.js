169ce8c456c86286779c67bb4a7e48a24fbd9ccb
Jake Dudum <jakedudum@gmail.com>
2019-06-09 16:23:28 -0700

    Initialized starter files and directories

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCDDshZBVe4v9dyD5lXvIeX3bm6YLXyyQY",
+    authDomain: "rps-multiplayer-9f587.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-9f587.firebaseio.com",
+    projectId: "rps-multiplayer-9f587",
+    storageBucket: "rps-multiplayer-9f587.appspot.com",
+    messagingSenderId: "221832212197",
+    appId: "1:221832212197:web:25093dabe74fa862"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
