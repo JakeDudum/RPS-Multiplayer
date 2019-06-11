e3d69fd877f33926bf0ad9f037131b82a7167bec
Jake Dudum <jakedudum@gmail.com>
2019-06-10 19:49:39 -0700

    Added functionality to firebase snapshots to adjust button displays for rock/paper/scissors based on whos turn it is currently and added a turn key to each player in game.js. Also dried up code and removed duplicate fucntion in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,20 +27,13 @@ var losses1 = 0;
 var losses2 = 0;
 var message = "";
 var messages = [];
+var turn;
 
 function whoWins() {
     result = Player1.pick - Player2.pick;
     if (result === -2 || result === 1) {
         wins1++;
         losses2++;
-
-        database.ref("player1").update({
-            wins: wins1
-        });
-
-        database.ref("player2").update({
-            losses: losses2
-        });
 
         database.ref("chat").push({
             message: "Player 1 WINS!!!"
@@ -50,29 +43,28 @@ function whoWins() {
         wins2++;
         losses1++;
 
-        database.ref("player2").update({
-            wins: wins2
-        });
-
-        database.ref("player1").update({
-            losses: losses1
-        });
-
         database.ref("chat").push({
             message: "Player 2 WINS!!!"
         })
     }
     else {
         database.ref("chat").push({
-            message: "It's a Tie!!!"
+            message: "It's a TIE!!!"
         })
     }
-    if (key1 === true) {
-        $("#choice1").removeClass("d-none");
-    } else {
-        $("#choice2").removeClass("d-none");
-    }
-    $("#messages").stop().animate({ scrollTop: $("#messages")[0].scrollHeight}, 1000);
+
+    database.ref("player1").update({
+        wins: wins1,
+        losses: losses1,
+        turn: true
+    });
+
+    database.ref("player2").update({
+        wins: wins2,
+        losses: losses2,
+        turn: false
+    });
+    $("#messages").stop().animate({ scrollTop: $("#messages")[0].scrollHeight }, 1000);
 }
 
 database.ref().on("value", function (snapshot) {
@@ -81,22 +73,28 @@ database.ref().on("value", function (snapshot) {
     $("#player1Wins").html(snapshot.child("player1").child("wins").val());
     $("#player1Losses").html(snapshot.child("player1").child("losses").val());
 
-    if (snapshot.child("player1").child("name").val() != "waiting") {
-        $("#join1").addClass("d-none");
-    }
-
     Player2 = snapshot.child("player2").val();
     $("#player2Name").html(snapshot.child("player2").child("name").val());
     $("#player2Wins").html(snapshot.child("player2").child("wins").val());
     $("#player2Losses").html(snapshot.child("player2").child("losses").val());
 
-    if (snapshot.child("player2").child("name").val() != "waiting") {
+    if (key1 === true && snapshot.child("player1").child("turn").val() === true) {
+        $("#choice1").removeClass("d-none");
+    }
+
+    if (key2 === true && snapshot.child("player2").child("turn").val() === true) {
+        $("#choice2").removeClass("d-none");
+    }
+
+    if (Player1 != null) {
+        $("#join1").addClass("d-none");
+    }
+    if (Player2 != null) {
         $("#join2").addClass("d-none");
     }
 
     var messageDiv = $("<div>");
     for (var key in snapshot.val().chat) {
-        console.log(snapshot.val().chat[key].message);
         var row = $("<p>").text(snapshot.val().chat[key].message);
         row.addClass("mb-0");
         messageDiv.append(row);
@@ -104,126 +102,148 @@ database.ref().on("value", function (snapshot) {
     $("#messages").html(messageDiv);
 });
 
-$("#submit").on('click', function () {
-    event.preventDefault();
-    message = $("#message").val().trim();
+$(document).ready(function () {
 
-    database.ref("chat").push({
-        message: message
-    })
-    $("#message").val("");
-    $("#messages").stop().animate({ scrollTop: $("#messages")[0].scrollHeight}, 1000);
-});
+    $("#submit").on('click', function () {
+        event.preventDefault();
+        if (key1 === true || key2 === true) {
+            message = $("#message").val().trim();
+
+            database.ref("chat").push({
+                message: message
+            })
+            $("#message").val("");
+            $("#messages").stop().animate({ scrollTop: $("#messages")[0].scrollHeight }, 1000);
+        }
+    });
 
 
-$("#player1").on('click', function () {
-    event.preventDefault();
+    $("#player1").on('click', function () {
+        event.preventDefault();
 
-    name = $("#inputName1").val().trim();
+        name = $("#inputName1").val().trim();
 
-    if (!Player1.exists) {
+        if (!key1 && name.length > 0) {
+            database.ref('/player1').update({
+                name: name,
+                exists: true,
+                wins: wins1,
+                losses: losses1,
+                turn: true
+            });
+            key1 = true;
+
+            $("#join1").addClass("d-none");
+            $("#join2").addClass("d-none");
+
+            $("#choice1").removeClass("d-none");
+        }
+        else {
+            database.ref("chat").push({
+                message: "Please enter a non-empty name."
+            })
+        }
+    });
+
+    $("#player2").on('click', function () {
+        event.preventDefault();
+
+        name = $("#inputName2").val().trim();
+
+        if (!key2 && name.length > 0) {
+            database.ref('/player2').update({
+                name: name,
+                exists: true,
+                wins: wins2,
+                losses: losses2,
+                turn: false
+            });
+            key2 = true;
+
+            $("#join1").addClass("d-none");
+            $("#join2").addClass("d-none");
+        }
+        else {
+            database.ref("chat").push({
+                message: "Please enter a non-empty name."
+            })
+        }
+
+    });
+
+    $("#rock1").on("click", function () {
+        event.preventDefault();
+
         database.ref("player1").update({
-            name: name,
-            exists: true,
+            pick: 1,
+            turn: false
         });
-        key1 = true;
-    }
-    $("#join1").addClass("d-none");
-    $("#join2").addClass("d-none");
+        $("#choice1").addClass("d-none");
 
-    $("#choice1").removeClass("d-none");
-});
-
-$("#player2").on('click', function () {
-    event.preventDefault();
-
-    name = $("#inputName2").val().trim();
-
-    if (!Player2.exists) {
         database.ref("player2").update({
-            name: name,
-            exists: true,
+            turn: true
         });
-        key2 = true;
-    }
-    $("#join1").addClass("d-none");
-    $("#join2").addClass("d-none");
+    })
 
-    $("#choice2").removeClass("d-none");
+    $("#paper1").on("click", function () {
+        event.preventDefault();
+
+        database.ref("player1").update({
+            pick: 2,
+            turn: false
+        });
+        $("#choice1").addClass("d-none");
+
+        database.ref("player2").update({
+            turn: true,
+        });
+    })
+
+    $("#scissors1").on("click", function () {
+        event.preventDefault();
+
+        database.ref("player1").update({
+            pick: 3,
+            turn: false
+        });
+        $("#choice1").addClass("d-none");
+
+        database.ref("player2").update({
+            turn: true
+        });
+    })
+
+    $("#rock2").on("click", function () {
+        event.preventDefault();
+
+        database.ref("player2").update({
+            pick: 1,
+            turn: false
+        });
+        $("#choice2").addClass("d-none");
+        whoWins();
+    })
+
+    $("#paper2").on("click", function () {
+        event.preventDefault();
+
+        database.ref("player2").update({
+            pick: 2,
+            turn: false
+        });
+        $("#choice2").addClass("d-none");
+        whoWins();
+    })
+
+    $("#scissors2").on("click", function () {
+        event.preventDefault();
+
+        database.ref("player2").update({
+            pick: 3,
+            turn: false
+        });
+        $("#choice2").addClass("d-none");
+        whoWins();
+    })
+
 });
-
-$("#rock1").on("click", function () {
-    event.preventDefault();
-
-    database.ref("player1").update({
-        pick: 1
-    });
-    $("#choice1").addClass("d-none");
-})
-
-$("#paper1").on("click", function () {
-    event.preventDefault();
-
-    database.ref("player1").update({
-        pick: 2
-    });
-    $("#choice1").addClass("d-none");
-})
-
-$("#scissors1").on("click", function () {
-    event.preventDefault();
-
-    database.ref("player1").update({
-        pick: 3
-    });
-    $("#choice1").addClass("d-none");
-})
-
-$("#rock2").on("click", function () {
-    event.preventDefault();
-
-    database.ref("player2").update({
-        pick: 1
-    });
-    $("#choice2").addClass("d-none");
-    whoWins();
-})
-
-$("#paper2").on("click", function () {
-    event.preventDefault();
-
-    database.ref("player2").update({
-        pick: 2
-    });
-    $("#choice2").addClass("d-none");
-    whoWins();
-})
-
-$("#scissors2").on("click", function () {
-    event.preventDefault();
-
-    database.ref("player2").update({
-        pick: 3
-    });
-    $("#choice2").addClass("d-none");
-    whoWins();
-})
-
-database.ref('/player1').set({
-    name: "waiting",
-    exists: false,
-    wins: wins1,
-    losses: losses1
-});
-
-database.ref('/player2').set({
-    name: "waiting",
-    exists: false,
-    wins: wins2,
-    losses: losses2
-});
-
-$("#submit").on('click', function () {
-    event.preventDefault();
-
-})
